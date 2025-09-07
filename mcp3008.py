c0551d2d9568580cc2552f3d724cb72f77b8eb90
Mohammed Nabeel <abdulmuminnabeelmohammed@gmail.com>
2025-09-07 15:35:33 +0100

    edits

--- mcp3008.py
+++ mcp3008.py
@@ -1,4 +1,4 @@
-print("Im  the ADC")
+print("Analog to Digital convertor")
 
 
 import spidev   
@@ -7,10 +7,10 @@ import time
 
 spi = spidev.SpiDev()       
 spi.open(0, 0)              
-spi.max_speed_hz = 1350000  
+spi.max_speed_hz = 1350000   # this is always on the datasheet
 
 
-def read_channel(channel):
+def channel_readings(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
@@ -24,7 +24,7 @@ def convert_to_voltage(data, vref=3.3):
 try:
     print("Reading LDR on CH0...")
     while True:
-        ldr_value = read_channel(0)             
+        ldr_value = channel_readings(0)             
         ldr_voltage = convert_to_voltage(ldr_value)
         print(f"LDR digital: {ldr_value}, Voltage: {ldr_voltage:.2f} V")
         time.sleep(1)  
---
+++ test.py
@@ -0,0 +1,6 @@
+import spidev
+import time
+
+spi = spi.SpiDev()
+spi.open(0,0)
+
