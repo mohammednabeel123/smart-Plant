3a38bb7d9ff88b479eee70809a9d54870cec7284
Mohammed Nabeel <abdulmuminnabeelmohammed@gmail.com>
2025-09-12 07:07:43 +0100

    edits

--- sensors.py
+++ sensors.py
@@ -2,10 +2,10 @@ print("I'm the sensors")
 
 import adafruit_dht
 
-sDHT_SENSOR = Adafruit_DHT.DHT22
+sDHT_SENSOR = adafruit_dht.DHT22
 DHT_PIN = 4  # BCM numbering, GPIO4
 
-humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+humidity, temperature = sDHT_SENSOR.read_retry(DHT_SENSOR, DHT_PIN)
 
 if humidity is not None and temperature is not None:
     print(f"Temp={temperature:.1f}C  Humidity={humidity:.1f}%")
